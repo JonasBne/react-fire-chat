4d190bd48df22727fafa93870e4467776efa4851
Jonas <boone.jonas@outlook.be>
2022-03-16 09:36:15 +0100

    build: added codegen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Chat from './views/chat/Chat';
+import Chat from './views/Chat/Chat';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Chat-0-matic</h1>
+      <h1 className="text-2xl font-bold underline">Chat-0-matic</h1>
       <Chat />
     </>
   );
