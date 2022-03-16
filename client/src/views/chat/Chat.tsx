d734bc88af43f3fb1e1e6b3b7b5df048591df17f
Jonas <boone.jonas@outlook.be>
2022-03-16 13:57:05 +0100

    style: basic UI for chat box

--- client/src/views/chat/Chat.tsx
+++ client/src/views/chat/Chat.tsx
@@ -1,8 +1,21 @@
+import styled from '@emotion/styled';
 import React from 'react';
 import Messages from './Messages';
 
+const ChatBox = styled.div({
+  margin: 'auto',
+  padding: '32px',
+  width: '50%',
+  borderRadius: '32px',
+  border: '1px solid black',
+});
+
 function Chat() {
-  return <Messages recipient="Jonas" />;
+  return (
+    <ChatBox>
+      <Messages recipient="Jonas" />
+    </ChatBox>
+  );
 }
 
 export default Chat;
