2d56d6e0dafd4fa29c46c886cdc625a6aa593d17
Jonas <boone.jonas@outlook.be>
2022-03-16 09:20:37 +0100

    refactor: changes to linter options

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline">Chat-0-matic</h1>;
 }
 
 export default App;
---
+++ client/src/views/Chat.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+function Chat() {
+  return <div>Chat window</div>;
+}
+
+export default Chat;
