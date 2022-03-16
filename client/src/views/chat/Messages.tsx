ba9418a477a1b4ef8787703f4fae74106a6ceea1
Jonas <boone.jonas@outlook.be>
2022-03-16 11:43:01 +0100

    build: added classnames package

--- client/src/views/chat/Messages.tsx
+++ client/src/views/chat/Messages.tsx
@@ -1,14 +1,30 @@
+/* eslint-disable react/require-default-props */
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { GetMessagesQuery } from '../../graphql/types';
 import { GET_MESSAGES } from '../../graphql/queries';
 
-function Messages() {
+// TODO: remove this optional character
+interface MessagesProps {
+  user?: string;
+}
+
+// TODO: remove this disable
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function Messages({ user = 'Jonas' }: MessagesProps) {
   const { data } = useQuery<GetMessagesQuery>(GET_MESSAGES);
 
   if (!data) return null;
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    // TODO: remove this disable
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {data.messages?.map((message) => (
+        <div>{message.content}</div>
+      ))}
+    </>
+  );
 }
 
 export default Messages;
