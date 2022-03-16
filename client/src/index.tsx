7437ed3048a3c088074c57bd959c5f14feebafcf
Jonas <boone.jonas@outlook.be>
2022-03-16 09:23:01 +0100

    refactor: changes to linter options

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
