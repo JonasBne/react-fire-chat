db42a5ce2b0080e6eb63ad53fb56dd1490875566
Jonas <boone.jonas@outlook.be>
2022-03-17 09:53:47 +0100

    refactor: finalized login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase';
+import Chat from './views/Chat/Chat';
 import SignIn from './views/SignIn';
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <>
       <h1 className="text-2xl font-bold underline">Chat-0-matic</h1>
-      <SignIn />
+      {user ? <Chat /> : <SignIn />}
     </>
   );
 }
--- client/src/views/SignIn.tsx
+++ client/src/views/SignIn.tsx
@@ -9,29 +9,29 @@ function SignIn() {
   const signInWithGoogle = async () => {
     const provider = new GoogleAuthProvider();
 
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential?.accessToken;
-        // The signed-in user info.
-        const { user } = result;
-        // ...
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const { email } = error;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-      });
+    signInWithPopup(auth, provider);
+    // .then((result) => {
+    //   // This gives you a Google Access Token. You can use it to access the Google API.
+    //   const credential = GoogleAuthProvider.credentialFromResult(result);
+    //   const token = credential?.accessToken;
+    //   // The signed-in user info.
+    //   const { user } = result;
+    //   // ...
+    // })
+    // .catch((error) => {
+    //   // Handle Errors here.
+    //   const errorCode = error.code;
+    //   const errorMessage = error.message;
+    //   // The email of the user's account used.
+    //   const { email } = error;
+    //   // The AuthCredential type that was used.
+    //   const credential = GoogleAuthProvider.credentialFromError(error);
+    //   // ...
+    // });
   };
 
   return (
-    <Button type="button" onClick={signInWithGoogle}>
+    <Button type="button" onClick={signInWithGoogle} variant="contained">
       Sign In With Google
     </Button>
   );
