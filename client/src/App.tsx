cf28f9e4217d9592e7229d60b6d3a17a9adbc6a6
Jonas <boone.jonas@outlook.be>
2022-03-17 09:20:24 +0100

    refactor: moved code into separate file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,5 @@
 import React from 'react';
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore/lite';
-
-import { getAuth } from 'firebase/auth';
-import 'firebase/firestore';
-// import { useAuthState } from 'react-firebase-hooks/auth';
-// import { useCollectionData } from 'react-firebase-hooks/firestore';
 import Chat from './views/Chat/Chat';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyD-gtcusMd6k2fpltaKA7YxsY3Oewr1b9o',
-  authDomain: 'chat-o-matic.firebaseapp.com',
-  databaseURL: 'https://chat-o-matic-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'chat-o-matic',
-  storageBucket: 'chat-o-matic.appspot.com',
-  messagingSenderId: '958313815470',
-  appId: '1:958313815470:web:8d6954773602e6ce8dbc3f',
-  measurementId: 'G-51QSBHXEB5',
-};
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-const firestoreDb = getFirestore(app);
 
 function App() {
   return (
---
+++ client/src/firebase.ts
@@ -0,0 +1,26 @@
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+
+import { getAuth } from 'firebase/auth';
+import 'firebase/firestore';
+// import { useAuthState } from 'react-firebase-hooks/auth';
+// import { useCollectionData } from 'react-firebase-hooks/firestore';
+import Chat from './views/Chat/Chat';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD-gtcusMd6k2fpltaKA7YxsY3Oewr1b9o',
+  authDomain: 'chat-o-matic.firebaseapp.com',
+  databaseURL: 'https://chat-o-matic-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'chat-o-matic',
+  storageBucket: 'chat-o-matic.appspot.com',
+  messagingSenderId: '958313815470',
+  appId: '1:958313815470:web:8d6954773602e6ce8dbc3f',
+  measurementId: 'G-51QSBHXEB5',
+};
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const database = firebase.database();
