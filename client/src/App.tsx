593b686d286cda7a2e7bab69cb448acc2a170360
Jonas <boone.jonas@outlook.be>
2022-03-17 11:17:05 +0100

    style: added first basic UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,26 +2,50 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import styled from '@emotion/styled';
 import { auth } from './firebase';
 import AuthenticatedApp from './views/AuthenticatedApp';
 import UnauthenticatedApp from './views/UnauthenticatedApp';
+
+const AppContainer = styled(Box)({
+  textAlign: 'center',
+  maxWidth: '728px',
+  height: '80vh',
+  margin: '24px',
+});
+
+const Header = styled.header({
+  color: 'white',
+  width: '100%',
+  maxWidth: '728px',
+  padding: '12px',
+});
+
+const Section = styled.section({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  minHeight: '100vh',
+  marginInline: '24px',
+});
+
+const Title = styled(Typography)({
+  fontWeight: 500,
+  textAlign: 'center',
+  letterSpacing: '1.5px',
+  textTransform: 'capitalize',
+});
 
 function App() {
   const [user] = useAuthState(auth);
 
   return (
-    <Box
-      sx={{
-        margin: '48px auto auto auto',
-        textAlign: 'center',
-        maxWidth: '780px',
-      }}
-    >
-      <Typography variant="h3" sx={{ fontWeight: 500 }}>
-        firechat 🔥
-      </Typography>
-      {user ? <AuthenticatedApp user={user} /> : <UnauthenticatedApp />}
-    </Box>
+    <AppContainer>
+      <Header>
+        <Title variant="h3">firechat 🔥</Title>
+      </Header>
+      <Section>{user ? <AuthenticatedApp user={user} /> : <UnauthenticatedApp />}</Section>
+    </AppContainer>
   );
 }
 
