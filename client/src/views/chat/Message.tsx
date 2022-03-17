b08062a9a5a66fc210e75b2b333af3aa41f733bc
Jonas <boone.jonas@outlook.be>
2022-03-17 15:15:40 +0100

    style: added styles for user image

--- client/src/views/chat/Message.tsx
+++ client/src/views/chat/Message.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import { auth } from '../../firebase';
 
 interface MessageProps {
@@ -7,12 +8,19 @@ interface MessageProps {
   photoUrl: string;
 }
 
+const UserImage = styled.img({
+  width: '40px',
+  height: '40px',
+  borderRadius: '50%',
+  margin: '2px 5px',
+});
+
 function Message({ content, userId, photoUrl }: MessageProps) {
   console.log('authenticated user', auth.currentUser);
   console.log('userid', userId);
   return (
     <div>
-      <img src={photoUrl ?? 'https://dummyimage.com/40x40/c72cc7/ffffff.png&text=U'} alt="user-avatar" />
+      <UserImage src={photoUrl ?? 'https://dummyimage.com/40x40/c72cc7/ffffff.png&text=U'} alt="user-avatar" />
       <p>{content}</p>
     </div>
   );
