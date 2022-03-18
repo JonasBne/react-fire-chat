802c59371b98b8d7dac925f1606467166a7d1c2b
Jonas <boone.jonas@outlook.be>
2022-03-18 09:38:44 +0100

    build: added package to handle subscriptions

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import './index.css';
 import App from './App';
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: 'ws://localhost:4000/subscriptions',
+  }),
+);
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
