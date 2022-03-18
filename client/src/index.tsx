f1112da1f31dd55bd6c105e6eb16b6c4ec909ab0
Jonas <boone.jonas@outlook.be>
2022-03-18 10:21:57 +0100

    refactor: added split link

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, split, HttpLink } from '@apollo/client';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 import './index.css';
 import App from './App';
 
@@ -12,8 +13,21 @@ const wsLink = new GraphQLWsLink(
   }),
 );
 
-const client = new ApolloClient({
+const httpLink = new HttpLink({
   uri: 'http://localhost:4000/graphql',
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
+
+const client = new ApolloClient({
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
